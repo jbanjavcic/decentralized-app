a77c0595ec4a3be606c930f7eda576978dc227b1
jbanjavcic <38728118+jbanjavcic@users.noreply.github.com>
2019-09-14 06:16:05 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -9,13 +9,10 @@ App = {
   },
 
   initWeb3: function() {
-    // TODO: refactor conditional
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
@@ -24,29 +21,20 @@ App = {
 
   initContract: function() {
     $.getJSON("Compete.json", function(compete) {
-      // Instantiate a new truffle contract from the artifact
       App.contracts.Compete = TruffleContract(compete);
-      // Connect provider to interact with contract
       App.contracts.Compete.setProvider(App.web3Provider);
-
       App.listenForEvents();
-
       return App.render();
     });
   },
 
-  // Listen for events emitted from the contract
   listenForEvents: function() {
     App.contracts.Compete.deployed().then(function(instance) {
-      // Restart Chrome if you are unable to receive this event
-      // This is a known issue with Metamask
-      // https://github.com/MetaMask/metamask-extension/issues/2393
       instance.votedEvent({}, {
         fromBlock: 0,
         toBlock: 'latest'
       }).watch(function(error, event) {
         console.log("event triggered", event)
-        // Reload when a new vote is recorded
         App.render();
       });
     });
@@ -60,7 +48,6 @@ App = {
     loader.show();
     content.hide();
 
-    // Load account data
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
@@ -68,7 +55,6 @@ App = {
       }
     });
 
-    // Load contract data
     App.contracts.Compete.deployed().then(function(instance) {
       competeInstance = instance;
       return competeInstance.competitorsCount();
@@ -85,11 +71,9 @@ App = {
           var name = competitor[1];
           var voteCount = competitor[2];
 
-          // Render candidate Result
           var competitorTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
           competitorsResults.append(competitorTemplate);
 
-          // Render candidate ballot option
           var competitorOption = "<option value='" + id + "' >" + name + "</ option>"
           competitorsSelect.append(competitorOption);
         });
@@ -112,7 +96,6 @@ App = {
     App.contracts.Compete.deployed().then(function(instance) {
       return instance.vote(competitorId, { from: App.account });
     }).then(function(result) {
-      // Wait for votes to update
       $("#content").hide();
       $("#loader").show();
     }).catch(function(err) {
